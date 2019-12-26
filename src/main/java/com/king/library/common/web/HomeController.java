d1fdf658a9b8b59712c7882003b97d224031e63d
king1024 <412207397@qq.com>
2019-12-26 16:47:18 +0800

    优化直接跳转页面的Controller

--- src/main/java/com/king/library/common/web/HomeController.java
+++ src/main/java/com/king/library/common/web/HomeController.java
@@ -1,5 +1,6 @@
 package com.king.library.common.web;
 
+import com.king.library.common.tools.StringTools;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,9 +13,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 
-    @RequestMapping("/page/{path}")
-    public String gotoView(@PathVariable String path){
-        System.out.println("==============================>>>> "+ path);
+    @RequestMapping(value={"/page/{path1}","/page/{path1}/{path2}","/page/{path1}/{path2}/{path3}",
+            "/page/{path1}/{path2}/{path3}/{path4}","/page/{path1}/{path2}/{path3}/{path4}/{path5}"} )
+    public String gotoView(@PathVariable(value="path1",required = true) String path1,
+                           @PathVariable(value="path2",required = false) String path2, @PathVariable(value="path3",required = false) String path3,
+                           @PathVariable(value="path4",required = false) String path4,@PathVariable(value="path5",required = false) String path5){
+        String path=path1;
+        if(StringTools.isNotEmpty(path2)){
+            path += "/"+path2;
+        }
+        if(StringTools.isNotEmpty(path3)){
+            path += "/"+path3;
+        }
+        if(StringTools.isNotEmpty(path4)){
+            path += "/"+path4;
+        }
+        if(StringTools.isNotEmpty(path5)){
+            path += "/"+path5;
+        }
         return path;
     }
 }
